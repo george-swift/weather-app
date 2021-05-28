58a789c1f83644bc8a59fa107c3d0fc11d5664e5
George <71434803+george-swift@users.noreply.github.com>
2021-05-28 18:57:54 +0100

    Add event listener and DOM manipulation functions to entry file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,67 @@
 import './style.css';
 import { manageNavbar, reportManager } from './modules/UIManager.js';
+import { dataFiller, searchValidator, celsiusToFahrenheit } from './modules/helpers.js';
+import weatherReport from './modules/weatherAPI.js';
+
+const handleSearch = () => {
+  const searchField = document.querySelector('#searchField');
+  const searchBtn = document.querySelector('#searchBtn');
+  const displayError = document.querySelector('#errorMessage');
+  const unit = document.querySelector('#temperature');
+
+  searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const userInput = searchField.value.trim();
+    if (searchValidator(userInput)) {
+      weatherReport(userInput)
+        .then((response) => {
+          dataFiller(response);
+          searchField.value = '';
+          unit.textContent = '°C';
+        })
+        .catch(() => {
+          // eslint-disable-next-line no-undef
+          $('#errorModal').modal('show');
+          displayError.textContent = 'City not found';
+          searchField.value = '';
+        });
+    } else {
+      // eslint-disable-next-line no-undef
+      $('#errorModal').modal('show');
+      displayError.textContent = 'Invalid input. Enter a valid city name';
+      searchField.value = '';
+    }
+  });
+};
+
+const changeUnit = () => {
+  const unitConverter = document.querySelector('.converter');
+  const unit = document.querySelector('#temperature');
+
+  unitConverter.addEventListener('click', () => {
+    if (unit.textContent === '°C' && localStorage.getItem('weather-report')) {
+      const weather = JSON.parse(localStorage.getItem('weather-report'));
+      dataFiller(weather);
+
+      const tempDisplays = document.getElementsByClassName('unit');
+      Array.from(tempDisplays).forEach((display) => {
+        const [temp] = display.textContent.split('°');
+        display.textContent = `${celsiusToFahrenheit(temp)}°`;
+      });
+
+      unit.textContent = '°F';
+    } else if (unit.textContent === '°F' && localStorage.getItem('weather-report')) {
+      const weather = JSON.parse(localStorage.getItem('weather-report'));
+      dataFiller(weather);
+      unit.textContent = '°C';
+    }
+  });
+};
 
 manageNavbar();
 
 reportManager();
+
+handleSearch();
+
+changeUnit();
